ea7e359f677d7f056a3b84b0ca85e8bd035a4dde
Roemer Naber <200611@student.glu.nl>
2021-05-31 15:45:16 +0200

    eigenschappen toegevoegd

--- controller_class.php
+++ controller_class.php
@@ -1,5 +1,5 @@
 <?php
-class controller{
+class GameController{
     public $_cable = 1.5 . "black cable";
     public $_lenght = 15.5 . "cm";
     public $_s_color = ["green", "red", "blue", "yellow"];
@@ -7,22 +7,26 @@ class controller{
     public $_depad = 1;
     public $_handgrip= 2;
     public $_logo=1;
+    public $_name="";
 
-    public function __construct($_cable,$_lenght,$_s_color,$_joysticks,$_depad,$_handgrip,$_logo){
-        $this->$_cable;
-        $this->$_lenght;
-        $this->$_s_color;
-        $this->$_joysticks;
-        $this->$_depad;
-        $this->$_handgrip;
-        $this->$_logo;
+    public function __construct($_name){
         
-        public function displayVar() {
-            echo $this->$_s_color;
-        }
+        $this->_name=$_name;
+    }
+
+    public function displayVar() {
+        echo $this->$_s_color;
+    }
+
 }
 
+echo "<pre>";
 
+$xbox360 = new GameController("xbox");
 
+var_dump($xbox360);
+
+$playstation = new GameController("playstation");
+var_dump($playstation);
 
 ?>
