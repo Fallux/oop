e526afe018b973ca42fa37dcfeb9cf15709b689a
Roemer Naber <200611@student.glu.nl>
2021-06-14 15:35:16 +0200

    cable privated en logo protected

--- controller_class.php
+++ controller_class.php
@@ -1,13 +1,13 @@
 <?php
 class GameController{
-    public $_cable = 1.5 . "black cable";
-    public $_lenght = 15.5 . "cm";
+    private $_cable = 1.5 . " " . "black cable";
+    public $_lenght = 15.5 . " " . "cm";
     // public $_s_color = ["green", "red", "blue", "yellow"];
     public $_josticks = 2;
     public $_depad = 1;
     public $_handgrip= 2;
-    public $_logo=1;
-    public $_name="";
+    protected $_logo=1;
+    public $_name="hello world";
     public function __construct($_name){
         
         $this->_name=$_name;
@@ -18,15 +18,18 @@ class GameController{
     }
 }
 class Button extends GameController{
-    public $_nameB;
+    public $_nameB="hello world";
     public $_color;
     public $_shape;
-    // public $_width;
-    // public $_height;
+    public $_width;
+    public $_height;
     public function __construct($_nameB, $_name){
-        parent::_construct($_name)
+        // parent::_construct($_name) dit werkt niet
         $this->_nameB = $_nameB;
-
+        
+    }
+    public function displayVar() {
+        // echo $this->$_s_color;
     }
 }
 echo "<pre>";
